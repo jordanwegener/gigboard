e58bd56ec007fc2401ef5a1b1fc3eb497a89c343
arbpotatoes <arbpotatoes@gmail.com>
2021-05-31 17:37:10 +1000

    CONTROLLER: removed erroneous code from applicationcontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,6 @@
 require "base64"
 
 class ApplicationController < ActionController::Base
-  rescue_from CanCan::AccessDenied do |exception|
-    flash.alert = "You do not have permission to do this. If you think this is an error, please contact an administrator."
-    redirect_to gigs_path
-  end
-
   def base64encode(file)
     str = File.read(file.tempfile)
     header = "data:audio/mp3;base64,"
